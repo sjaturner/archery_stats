532e2b89aa98cd96cf9e0ff48746fd3a5d35c192
Simon Turner <simon.turner@cambiumnetworks.com>
2020-05-15 13:22:27 +0100

    added a scoring approximation

--- post.m
+++ post.m
@@ -11,12 +11,13 @@ for bound = 40:20:200
    drawCircle(0, 0, bound, 'k');
 end
 points=points(4:end,:);
+score = 10 * (1 - sum((points / 200).^2,2));
 plot(points(:,1), points(:,2),'ro');
 m=mean(points);
 plot(m(1,1), m(1,2), 'go');
 s=std(points);
 drawEllipse(m(1,1), m(1,2), s(1,1), s(1,2), 'b');
-summary=sprintf("mean %d %d std %d %d", round(m(1,1)), round(m(1,2)), round(s(1,1)), round(s(1,2)))
+summary=sprintf("mean %d %d std %d %d score %f", round(m(1,1)), round(m(1,2)), round(s(1,1)), round(s(1,2)), sum(score) / 50)
 title(summary);
 print(sprintf("%s.png", argv(){1}))
 save(sprintf("%s.stats", argv(){1}));
