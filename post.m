3cec4d89943e9b3cae55f71e046a474ef03c7afa
Simon Turner <simon.turner@cambiumnetworks.com>
2020-06-28 14:26:54 +0100

    take out some of the perspective distortion by using a quadratic, now need to lead in with five calibration points

--- post.m
+++ post.m
@@ -1,23 +1,37 @@
 points=load(argv(){1});
 points(:,2)*=-1;
 points-=points(1,:);
-points(:,1)=points(:,1)/points(3,1)*200;points(:,2)=points(:,2)/points(2,2)*200;points=floor(points);
+
+maxy = points(2,2)
+maxx = points(3,1)
+miny = points(4,2)
+minx = points(5,1)
+
+px=polyfit([minx, 0, maxx], [-200, 0, 200], 2);
+points(:,1)=polyval(px, points(:,1)')'
+
+py=polyfit([miny, 0, maxy], [-200, 0, 200], 2);
+points(:,2)=polyval(py, points(:,2)')'
+
+points=floor(points);
 
 pkg load geometry
 
 hold on
 axis("equal")
-for bound = 40:20:200
+for bound = 20:20:200
    drawCircle(0, 0, bound, 'k');
 end
-points=points(4:end,:);
-score = 10 * (1 - sum((points / 200).^2,2));
+
+score = 1 + 10 * (1 - ((sum(points.^2,2).^.5)/200))
+card = floor(score)
+
 plot(points(:,1), points(:,2),'ro');
 m=mean(points);
 plot(m(1,1), m(1,2), 'go');
 s=std(points);
 drawEllipse(m(1,1), m(1,2), s(1,1), s(1,2), 'b');
-summary=sprintf("mean %d %d std %d %d score %f", round(m(1,1)), round(m(1,2)), round(s(1,1)), round(s(1,2)), mean(score))
+summary=sprintf("mean %d %d std %d %d mean %f card %d rounds %d", round(m(1,1)), round(m(1,2)), round(s(1,1)), round(s(1,2)), mean(score), sum(card), size(score)(1))
 title(summary);
 print(sprintf("%s.png", argv(){1}))
 save(sprintf("%s.stats", argv(){1}));
